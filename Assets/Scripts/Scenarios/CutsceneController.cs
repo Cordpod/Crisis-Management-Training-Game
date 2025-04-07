3bd711b490ecd050a22622729c11712e6127904c
cord <cordeliatxe@gmail.com>
2025-04-07 16:31:04 +0800

    theres some changes to the scenes, some changes to accomodate the animation and sounds in btw scenes, but doesnt fully work bc the options are clashing. trying to implement for training portion first

---
+++ Assets/Scripts/Scenarios/CutsceneController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public static CutsceneController instance;
+    public Transform trainTransform;
+    public Vector3 endPosition;
+    public float duration = 2f;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public IEnumerator MoveTrain()
+    {
+        Vector3 startPos = trainTransform.position;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            trainTransform.position = Vector3.Lerp(startPos, endPosition, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        trainTransform.position = endPosition;
+    }
+}
